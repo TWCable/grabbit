2cfb15782efdafb828586e49c8448db00e8f60ca
Sagar Sane <sagar.sane@i-cubed.com>
2014-11-11 22:56:19 -0700

    Better grabbit.sh

--- grabbit.sh
+++ grabbit.sh
@@ -2,20 +2,46 @@
 
 # Simple Script to initialize one or more Sync or Grab jobs for one or more paths
 # Sample usage :
-# Initialize : grabbit.sh init http://localhost:4502 admin admin config.json
-# Status : grabbit.sh status http://localhost:4502 admin admin <id1>,<id2>,..
+# sh grabbit.sh
+# Follow along to enter configuration information
+# Follow along to get status of the jobs
 
 #### Constants
 
-INIT="init" #init => Initialize Sync for config file
-STATUS="status" #status => Get Status for given JobIds delimited by commas
 INIT_URL="/bin/twc/client/grab"
 STATUS_URL="/bin/twc/client/grab/status"
 
-if [ $1 == $INIT ]; then
-    json=`curl -X POST -H "Content-Type: application/json" -d "@$5" -u $3:$4 $2$INIT_URL`
-    echo $json
-elif [ $1 == $STATUS ]; then
-    json=`curl -u $3:$4 --request GET $2$STATUS_URL"?jobIds="$5`
-    echo $json
-fi
+echo "Enter Client [http(s)://server:port] to initiate Grabbit request "
+read client
+
+echo
+echo "Client Username :"
+read username
+echo
+echo "Client Password :"
+read password
+
+echo
+echo "Enter path to your Grabbit Configuration file"
+read configpath
+
+# Kick off Grabbit jobs
+initResponse=`curl -X POST -H "Content-Type: application/json" -d "@$configpath" -u $username:$password $client$INIT_URL`
+clear
+echo "~~~~~Client configuration : $client $username $password Config: $configpath~~~~~"
+
+# Monitor Job Status
+while true; do
+	echo
+	echo "~~~~~~~~~~~Jobs kicked off : $initResponse ~~~~~~~~~~~~~"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	read -p "Enter Job Id(s) to get job status or enter 'q' to quit: " ids
+	if [ "$ids" == "q" ]; then
+		exit 0;
+	fi
+	statusJson=`curl -u $username:$password --request GET $client$STATUS_URL"?jobIds="$ids`
+	echo
+	echo "Status for JobIds [$ids] is :"
+	echo "$statusJson" | python -m json.tool 
+
+done
