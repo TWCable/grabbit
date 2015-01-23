e4267b5d0aaf3be73b2a81f4d426ff5d2338c1b5
Sagar Sane <sagar.sane@i-cubed.com>
2015-01-23 00:01:52 -0500

    WEBCMS-14417 : Cleanup
    * Better GrabbitConfiguration error handling
    * More Tests

--- grabbit/src/main/groovy/com/twc/grabbit/GrabbitConfiguration.groovy
+++ grabbit/src/main/groovy/com/twc/grabbit/GrabbitConfiguration.groovy
@@ -1,5 +1,6 @@
 package com.twc.grabbit
 
+import com.google.common.collect.ImmutableMap
 import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
@@ -18,10 +19,8 @@ class GrabbitConfiguration {
     Collection<PathConfiguration> pathConfigurations
 
     private GrabbitConfiguration(@Nonnull String user, @Nonnull String pass, @Nonnull String host,
-                                 @Nonnull String port, @Nonnull boolean deltaContent, @Nonnull Collection<PathConfiguration> pathConfigs) {
-        if(!user || !pass || !host || !port || !pathConfigs || (deltaContent == null)) {
-            throw new IllegalArgumentException("One of more configurations are null or empty. Please verify your configs")
-        }
+                                 @Nonnull String port, boolean deltaContent, @Nonnull Collection<PathConfiguration> pathConfigs) {
+        // all input is being verified by the "create" factory method
         this.serverUsername = user
         this.serverPassword = pass
         this.serverHost = host
@@ -35,21 +34,89 @@ class GrabbitConfiguration {
 
         final configMap = new JsonSlurper().parseText(configJson) as Map<String, String>
 
-        final Collection<PathConfiguration> pathConfigurations = configMap["pathConfigurations"]?.collect { config ->
-            def path = config["path"] as String ?: ""
+        def errorBuilder = ConfigurationException.builder()
+
+        def serverUsername = nonEmpty(configMap, 'serverUsername', errorBuilder)
+        def serverPassword = nonEmpty(configMap, 'serverPassword', errorBuilder)
+        def serverHost = nonEmpty(configMap, 'serverHost', errorBuilder)
+        def serverPort = nonEmpty(configMap, 'serverPort', errorBuilder)
+        def deltaContent = boolVal(configMap, 'deltaContent')
+
+        final Collection<PathConfiguration> pathConfigurations = configMap["pathConfigurations"]?.collect { Map config ->
+            def path = nonEmpty(config, 'path', errorBuilder)
             def workflowConfigIds = config["workflowConfigIds"] as Collection<String> ?: [] as Collection<String>
             new PathConfiguration(path, workflowConfigIds)
         } ?: [] as Collection<PathConfiguration>
 
-        new GrabbitConfiguration(
-            configMap["serverUsername"] as String ?: "",
-            configMap["serverPassword"] as String ?: "",
-            configMap["serverHost"] as String ?: "",
-            configMap["serverPort"] as String ?: "",
-            configMap["deltaContent"] as boolean ?: false,
-            pathConfigurations
-        )
+        if (pathConfigurations.isEmpty()) errorBuilder.add('pathConfigurations', 'is empty')
 
+        if (errorBuilder.hasErrors()) throw errorBuilder.build()
+
+        return new GrabbitConfiguration(
+                serverUsername,
+                serverPassword,
+                serverHost,
+                serverPort,
+                deltaContent,
+                pathConfigurations
+        )
+    }
+
+    private static String nonEmpty(Map<String, String> configMap, String key,
+                                   ConfigurationException.Builder errorBuilder) {
+        if (configMap.containsKey(key)) {
+            def val = configMap.get(key)
+            if (val.isAllWhitespace())
+                errorBuilder.add(key, 'is empty')
+            return val
+        } else {
+            errorBuilder.add(key, 'is missing')
+            return null
+        }
+    }
+
+    private static boolean boolVal(Map<String, String> configMap, String key) {
+        if(!configMap.containsKey(key)) {
+            log.warn "Input doesn't contain ${key} for a boolean value. Will default to false"
+        }
+        def boolVal = configMap.get(key) as boolean
+        return boolVal
+    }
+
+    @CompileStatic
+    static class ConfigurationException extends RuntimeException {
+        final Map<String, String> errors
+
+        ConfigurationException(@Nonnull String msg, @Nonnull Map<String, String> errors) {
+            super(msg)
+            this.errors = errors
+        }
+
+        static Builder builder() {
+            return new Builder()
+        }
+
+        @CompileStatic
+        static class Builder {
+            private ImmutableMap.Builder mapBuilder = ImmutableMap.builder()
+            private Map map
+
+            ConfigurationException build() {
+                if (map == null) map = mapBuilder.build()
+                return new ConfigurationException("Errors: ${map}", map)
+            }
+
+            Builder add(String keyName, String errorMessage) {
+                if (map != null) throw new IllegalStateException("Can't continue to add after building or checking for errors")
+                mapBuilder.put(keyName, errorMessage)
+                return this
+            }
+
+            boolean hasErrors() {
+                map = mapBuilder.build()
+                return !map.isEmpty()
+            }
+        }
     }
 
     @CompileStatic
@@ -58,8 +125,6 @@ class GrabbitConfiguration {
         Collection<String> workflowConfigIds
 
         protected PathConfiguration(@Nonnull String path, @Nonnull Collection<String> workflowConfigIds) {
-            if(!path) throw new IllegalArgumentException("path == null or empty")
-            if(workflowConfigIds == null) throw new IllegalArgumentException("workflowConfigIds == null")
             this.path = path
             this.workflowConfigIds = workflowConfigIds
         }
--- grabbit/src/test/groovy/com/twc/grabbit/client/GrabbitConfigurationSpec.groovy
+++ grabbit/src/test/groovy/com/twc/grabbit/client/GrabbitConfigurationSpec.groovy
@@ -3,19 +3,107 @@ package com.twc.grabbit.client
 import com.twc.grabbit.GrabbitConfiguration
 import spock.lang.Specification
 import spock.lang.Subject
+import spock.lang.Unroll
 
 @Subject(GrabbitConfiguration)
 class GrabbitConfigurationSpec extends Specification {
 
+    @Unroll
     def "Should create configuration from json input"() {
-        given:
-        def input = """
+        when:
+        def output = GrabbitConfiguration.create(input)
+
+        then:
+        output instanceof GrabbitConfiguration
+        output.deltaContent == false
+
+        where:
+        input << [
+            """
+            {
+                "serverUsername" : "admin",
+                "serverPassword" : "admin",
+                "serverHost" : "localhost",
+                "serverPort" : "4503",
+                "deltaContent" : false,
+                "pathConfigurations" :  [
+                    {
+                        "path" : "/content/residential-admin/checkout-mocks",
+                        "workflowConfigIds" : []
+                    },
+                    {
+                        "path" : "/content/residential-admin/ProductCatalog",
+                        "workflowConfigIds" :
+                        [
+                            "/etc/workflow/launcher/config/update_asset_mod",
+                            "/etc/workflow/launcher/config/update_asset_create",
+                            "/etc/workflow/launcher/config/dam_xmp_nested_writeback",
+                            "/etc/workflow/launcher/config/dam_xmp_writeback"
+                        ]
+                    },
+                    {
+                        "path" : "/content/dam/business/images",
+                        "workflowConfigIds" :
+                        [
+                            "something"
+                        ]
+                    }
+                ]
+            }
+            """,
+            """
+            {
+                "serverUsername" : "admin",
+                "serverPassword" : "admin",
+                "serverHost" : "localhost",
+                "serverPort" : "4503",
+                "pathConfigurations" :  [
+                    {
+                        "path" : "/content/residential-admin/checkout-mocks",
+                        "workflowConfigIds" : []
+                    },
+                    {
+                        "path" : "/content/residential-admin/ProductCatalog",
+                        "workflowConfigIds" :
+                        [
+                            "/etc/workflow/launcher/config/update_asset_mod",
+                            "/etc/workflow/launcher/config/update_asset_create",
+                            "/etc/workflow/launcher/config/dam_xmp_nested_writeback",
+                            "/etc/workflow/launcher/config/dam_xmp_writeback"
+                        ]
+                    },
+                    {
+                        "path" : "/content/dam/business/images",
+                        "workflowConfigIds" :
+                        [
+                            "something"
+                        ]
+                    }
+                ]
+            }
+            """
+        ]
+    }
+
+    @Unroll
+    def "Should fail to create configuration from json input"() {
+        when:
+        GrabbitConfiguration.create(input)
+
+        then:
+        final GrabbitConfiguration.ConfigurationException  exception = thrown()
+        exception.errors == errors
+
+
+        where:
+
+        input                                                                   | errors
+        """
         {
-            "serverUsername" : "admin",
             "serverPassword" : "admin",
             "serverHost" : "localhost",
             "serverPort" : "4503",
-            "deltaContent" : true,
+            "deltaContent" : false,
             "pathConfigurations" :  [
                 {
                     "path" : "/content/residential-admin/checkout-mocks",
@@ -40,13 +128,37 @@ class GrabbitConfigurationSpec extends Specification {
                 }
             ]
         }
+        """                                                                         | [ serverUsername : "is missing" ]
         """
-        when:
-        def output = GrabbitConfiguration.create(input)
+        {
+            "serverUsername" : "admin",
+            "serverPassword" : "admin",
+            "serverHost" : "localhost",
+            "pathConfigurations" :  [
+                {
+                },
+                {
+                    "path" : "/content/residential-admin/ProductCatalog",
+                    "workflowConfigIds" :
+                    [
+                        "/etc/workflow/launcher/config/update_asset_mod",
+                        "/etc/workflow/launcher/config/update_asset_create",
+                        "/etc/workflow/launcher/config/dam_xmp_nested_writeback",
+                        "/etc/workflow/launcher/config/dam_xmp_writeback"
+                    ]
+                },
+                {
+                    "path" : "/content/dam/business/images",
+                    "workflowConfigIds" :
+                    [
+                        "something"
+                    ]
+                }
+            ]
+        }
+        """                                                                         | [ serverPort : "is missing",
+                                                                                        path : "is missing" ]
 
-        then:
-        output instanceof GrabbitConfiguration
-        output.deltaContent == true
 
     }
 
