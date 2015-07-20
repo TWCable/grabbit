4844d3d0b41b567540039a4aa0ff972d1d32da42
Jeff Bornemann <bornemannjs@gmail.com>
2015-07-20 14:53:16 -0600

    Added missing copyright headers

--- grabbit/src/main/groovy/com/twcable/grabbit/jcr/ProtoNodeDecorator.groovy
+++ grabbit/src/main/groovy/com/twcable/grabbit/jcr/ProtoNodeDecorator.groovy
@@ -1,5 +1,21 @@
 package com.twcable.grabbit.jcr
 
+/*
+ * Copyright 2015 Time Warner Cable, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.twcable.grabbit.proto.NodeProtos.Node as ProtoNode
 import com.twcable.grabbit.proto.NodeProtos.Value as ProtoValue
 import groovy.transform.CompileStatic
