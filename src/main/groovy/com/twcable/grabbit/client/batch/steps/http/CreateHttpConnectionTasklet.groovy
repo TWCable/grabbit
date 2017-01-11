1ffe73f087204075e48901641abf0fb070d63b2a
Jeff Bornemann <jeff.bornemann@i-cubed.com>
2017-01-11 15:56:08 +0700

    Migrate HTTP client over to okhttp

--- src/main/groovy/com/twcable/grabbit/client/batch/steps/http/CreateHttpConnectionTasklet.groovy
+++ src/main/groovy/com/twcable/grabbit/client/batch/steps/http/CreateHttpConnectionTasklet.groovy
@@ -17,34 +17,30 @@ package com.twcable.grabbit.client.batch.steps.http
 
 import com.twcable.grabbit.client.batch.ClientBatchJob
 import com.twcable.grabbit.client.batch.ClientBatchJobContext
+import groovy.transform.Canonical
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import org.apache.http.HttpEntity
-import org.apache.http.HttpHost
-import org.apache.http.auth.AuthScope
-import org.apache.http.auth.UsernamePasswordCredentials
-import org.apache.http.client.AuthCache
-import org.apache.http.client.CredentialsProvider
-import org.apache.http.client.HttpClient
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.protocol.HttpClientContext
-import org.apache.http.client.utils.URIBuilder
-import org.apache.http.impl.auth.BasicScheme
-import org.apache.http.impl.client.BasicAuthCache
-import org.apache.http.impl.client.BasicCredentialsProvider
-import org.apache.http.impl.client.DefaultHttpClient
+import java.util.concurrent.TimeUnit
+import javax.annotation.Nonnull
+import okhttp3.Authenticator
+import okhttp3.Credentials
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
 import org.springframework.batch.core.ExitStatus
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 
-import javax.annotation.Nonnull
 
 import static javax.servlet.http.HttpServletResponse.SC_OK
+import static okhttp3.HttpUrl.Builder as HttpUrlBuilder
 
 /**
- * This tasklet opens an HTTP session for the current job, and places the input stream on the {@link ClientBatchJobContext}
+ * This tasklet opens an HTTP connection for the current job, and places the input stream on the {@link ClientBatchJobContext}
  */
 @CompileStatic
 @Slf4j
@@ -61,95 +57,77 @@ class CreateHttpConnectionTasklet implements Tasklet {
     RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
         final Map jobParameters = chunkContext.stepContext.jobParameters
 
-        final URI requestURI = buildURIForRequest(jobParameters)
+        Request request = new Request.Builder()
+            .url(getURLForRequest(jobParameters))
+            .build()
 
-        final String username = jobParameters.get(ClientBatchJob.SERVER_USERNAME)
-        final String password = jobParameters.get(ClientBatchJob.SERVER_PASSWORD)
-        final client = getHttpClient(username, password)
+        final String username = (String)jobParameters.get(ClientBatchJob.SERVER_USERNAME)
+        final String password = (String)jobParameters.get(ClientBatchJob.SERVER_PASSWORD)
 
-        //This allows us to authenticate against the Grabbit server preemptively
-        final httpClientContext = getPreemptiveAuthContext(requestURI)
-        final get = new HttpGet(requestURI)
-        final response = client.execute(get, httpClientContext)
+        final Connection connection = createConnection(request, username, password)
 
-        final int statusCode = response.getStatusLine().statusCode
-        if(statusCode != SC_OK) {
-            log.warn "Received a status of ${statusCode} when attempting to create a connection to ${jobParameters.get(ClientBatchJob.HOST)}. Bailing out. See debug log for more details."
-            log.debug "Request to start a connection with ${jobParameters.get(ClientBatchJob.HOST)} resulted in: ${response.entity}."
+        if(connection.status != SC_OK) {
+            log.warn "Received a status of ${connection.status} when attempting to create a connection to ${jobParameters.get(ClientBatchJob.HOST)}. Bailing out. See debug log for more details."
+            log.debug "Request to start a connection with ${jobParameters.get(ClientBatchJob.HOST)} resulted in: ${connection.networkResponse}."
             contribution.setExitStatus(ExitStatus.FAILED)
             return RepeatStatus.FINISHED
         }
-        HttpEntity responseEntity = response.entity
-        ClientBatchJobContext.setInputStream(responseEntity.content)
+
+        ClientBatchJobContext.setInputStream(connection.inputStream)
         return RepeatStatus.FINISHED
     }
 
-    /**
-     * Takes Grabbit job parameters and builds a URI for streaming content from server.
-     * @param jobParameters A Grabbit job's parameters. No validation for parameters at this point.
-     * @return {@link URI} for pulling a stream of Grabbit content.
-     */
-    private URI buildURIForRequest(@Nonnull final Map jobParameters) {
-        final String path = jobParameters.get(ClientBatchJob.PATH)
+
+    Connection createConnection(@Nonnull final Request request, @Nonnull final String username, @Nonnull final String password) {
+        final OkHttpClient client = new OkHttpClient.Builder()
+                                            .authenticator(
+                                                new Authenticator() {
+                                                    @Override
+                                                    Request authenticate(Route route, Response response) throws IOException {
+                                                        return response.request().newBuilder()
+                                                                .addHeader('Authorization', Credentials.basic(username, password))
+                                                                .build()
+                                                    }
+                                                }
+                                            )
+                                            .connectTimeout(1L, TimeUnit.MINUTES)
+                                            .readTimeout(1L, TimeUnit.MINUTES)
+                                            .writeTimeout(1L, TimeUnit.MINUTES)
+                                            .retryOnConnectionFailure(true)
+                                            .build()
+        final Response response = client.newCall(request).execute()
+        //We return response information in a connection like this because it's clear, but also because Response is a final class that we can not easily mock
+        return new Connection(response.body().byteStream(), response.networkResponse(), response.code())
+    }
+
+
+    HttpUrl getURLForRequest(@Nonnull final Map jobParameters) {
+        HttpUrlBuilder urlBuilder = new HttpUrl.Builder()
+
+        urlBuilder.scheme((String)jobParameters.get(ClientBatchJob.SCHEME))
+        urlBuilder.host((String)jobParameters.get(ClientBatchJob.HOST))
+        urlBuilder.port(Integer.parseInt((String)jobParameters.get(ClientBatchJob.PORT)))
+        urlBuilder.encodedPath('/grabbit/content')
+
+        //addQueryParameter will encode these values for us
+        urlBuilder.addQueryParameter('path', (String)jobParameters.get(ClientBatchJob.PATH))
+        urlBuilder.addQueryParameter('after', (String)jobParameters.get(ClientBatchJob.CONTENT_AFTER_DATE) ?: '')
+
         final String excludePathParam = jobParameters.get(ClientBatchJob.EXCLUDE_PATHS)
         final excludePaths = (excludePathParam != null && !excludePathParam.isEmpty() ? excludePathParam.split(/\*/) : Collections.EMPTY_LIST) as Collection<String>
-        final String scheme = jobParameters.get(ClientBatchJob.SCHEME)
-        final String host = jobParameters.get(ClientBatchJob.HOST)
-        final String port = jobParameters.get(ClientBatchJob.PORT)
-        final String contentAfterDate = jobParameters.get(ClientBatchJob.CONTENT_AFTER_DATE) ?: ""
-
-        final String encodedContentAfterDate = URLEncoder.encode(contentAfterDate, 'utf-8')
-        final String encodedPath = URLEncoder.encode(path, 'utf-8')
-
-        URIBuilder uriBuilder = new URIBuilder(scheme: scheme, host: host, port: port as Integer, path: "/grabbit/content")
-        uriBuilder.addParameter("path", encodedPath)
-        uriBuilder.addParameter("after", encodedContentAfterDate)
         for(String excludePath : excludePaths) {
-            uriBuilder.addParameter("excludePath", URLEncoder.encode(excludePath, 'UTF-8'))
+            urlBuilder.addQueryParameter('excludePath', excludePath)
         }
-        return uriBuilder.build()
+
+        return urlBuilder.build()
     }
 
-    /**
-     * Provides an HTTP client, complete with authentication credentials
-     * @param username to authenticate the request
-     * @param password to authenticate the request
-     * @return a {@link HttpClient} instance for making Grabbit requests
-     */
-     HttpClient getHttpClient(final String username, final String password) {
-        final client = getHttpClient() as DefaultHttpClient
 
-        CredentialsProvider credentialsProvider = new BasicCredentialsProvider()
-        credentialsProvider.setCredentials(
-                new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
-                new UsernamePasswordCredentials(username, password)
-        )
-        client.setCredentialsProvider(credentialsProvider)
-        return client
-    }
-
-    /**
-     * Provides a bare-bones HTTP client
-     * @return a {@link HttpClient} instance for making Grabbit requests
-     */
-     HttpClient getHttpClient() {
-        return new DefaultHttpClient()
-    }
-
-    /**
-     * Provides a HttpClientContext for preemptive authentication
-     * @param uri The Grabbit request URI to associate with the preemptive authentication
-     * @return {@link HttpClientContext} for preemptive authentication
-     */
-    private HttpClientContext getPreemptiveAuthContext(@Nonnull final URI uri) {
-        // Setup preemptive Authentication
-        // Create AuthCache instance
-        AuthCache authCache = new BasicAuthCache()
-        HttpHost host = new HttpHost(uri.host, uri.port, uri.scheme)
-        authCache.put(host, new BasicScheme())
-        // Add AuthCache to the execution context
-        HttpClientContext context = HttpClientContext.create()
-        context.setAuthCache(authCache)
-        return context
+    @CompileStatic
+    @Canonical
+    static class Connection {
+        InputStream inputStream
+        Response networkResponse
+        int status
     }
 }
