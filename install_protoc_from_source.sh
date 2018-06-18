2915a3bc883273f563f6bf47c289f8e9632ce735
Jeff Bornemann <jeff.bornemann@oracle.com>
2018-06-18 11:35:45 -0400

    Small modifications to install_protoc_from_source

--- install_protoc_from_source.sh
+++ install_protoc_from_source.sh
@@ -5,7 +5,7 @@ PROTOC_VERSION="2.6.1"
 if [[ ! -d "$HOME/protobuf/lib" ||  "$(protoc --version)" != *${PROTOC_VERSION}* ]]; then
   pushd .
   cd /tmp
-  wget https://github.com/google/protobuf/releases/download/v${PROTOC_VERSION}/protobuf-${PROTOC_VERSION}.tar.gz
+  wget --no-check-certificate https://github.com/google/protobuf/releases/download/v${PROTOC_VERSION}/protobuf-${PROTOC_VERSION}.tar.gz
   tar -xzvf protobuf-${PROTOC_VERSION}.tar.gz
   cd protobuf-${PROTOC_VERSION}
   ./configure --prefix=$HOME/protobuf
@@ -16,6 +16,9 @@ if [[ ! -d "$HOME/protobuf/lib" ||  "$(protoc --version)" != *${PROTOC_VERSION}*
   rm -rf protobuf-${PROTOC_VERSION}
   rm -f protobuf-${PROTOC_VERSION}.zip
   popd
+  echo "Add $HOME/protobuf/bin to your path!"
+  echo "On *nix:"
+  echo "echo 'PATH=\$PATH:~/protobuf/bin' >> ~/.bash_profile"
 else
   echo "$(protoc --version) already installed"
 fi
